ae341eafbdd921686cf9ad113461485a50766bd9
KiRp1cH <vadkit2018@gmail.com>
2021-06-02 14:22:00 +0300

    Hot fix cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_myapp_session", domain: all, tld:length:2
+Rails.application.config.session_store :cookie_store, key: "staff_session", domain: :all, tld_length: 2
