b9b4e0bd7b0db894b542f053767c99bdf673eb43
KiRp1cH <vadkit2018@gmail.com>
2021-06-06 02:20:58 +0300

    Updated interface

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :staffs
   #get 'home/about'
   get 'home/about'
-  root 'home#index'
+  #root 'home#index'
+  root 'staffs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
